e209252e50e0923f8c939fa4b8a26404b51d2231
Suhas K J <suhas.kj@ADKPISOFT.COM>
2021-10-26 19:46:56 +0530

    Revert "Revert "Fixed Edit Api""
    This reverts commit c039c36afd93e0e11d9177b5618fb46d28f685ce.

--- src/main/java/com/blogs/finalBlogs/BlogResource.java
+++ src/main/java/com/blogs/finalBlogs/BlogResource.java
@@ -40,7 +40,7 @@ public class BlogResource {
 
     }
 
-    @PutMapping("/")
+    @PutMapping("/update")
     public ResponseEntity<Blog> updateBlog(@RequestBody Blog blog) {
         Blog newBlog = blogService.updateBlogs(blog);
         return new ResponseEntity<>(newBlog, HttpStatus.OK);
