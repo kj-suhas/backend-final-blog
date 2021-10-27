1a0c217a227cd474327a11a0e01df93c33f9b6ec
Suhas K J <suhas.kj@ADKPISOFT.COM>
2021-10-27 15:07:35 +0530

    Added comments

--- src/main/java/com/blogs/finalBlogs/BlogResource.java
+++ src/main/java/com/blogs/finalBlogs/BlogResource.java
@@ -2,6 +2,7 @@ package com.blogs.finalBlogs;
 
 import java.util.List;
 
+import com.blogs.finalBlogs.exceptions.BadRequest;
 import com.blogs.finalBlogs.exceptions.ResourceNotFoundException;
 import com.blogs.finalBlogs.models.Blog;
 import com.blogs.finalBlogs.models.service.BlogService;
@@ -29,13 +30,19 @@ public class BlogResource {
         this.blogService = blogService;
     }
 
+
+    // @api:           "blog/all"
+    // @desc:         "Api to get all blogs"
+    // @access_type:  "Public"
     @GetMapping("/all")
     public ResponseEntity<List<Blog>> getAllAnimeList() {
         List<Blog> blogs = blogService.getBlogs();
         return new ResponseEntity<>(blogs, HttpStatus.OK);
-        // throw new ResourceNotFoundException("This is a test to see if i can see a response entity in browser");
     }
 
+    // @api:           "blog/add"
+    // @desc:         "Api add single blog"
+    // @access_type:  "Public"
     @PostMapping("/add")
     public ResponseEntity<Blog> addBlog(@RequestBody Blog blog) {
         Blog newBlog = blogService.addBlog(blog);
@@ -43,16 +50,23 @@ public class BlogResource {
 
     }
 
+    // @api:           "blog/update"
+    // @desc:         "Api to update/edit blogs"
+    // @access_type:  "Public"
     @PutMapping("/update")
     public ResponseEntity<Blog> updateBlog(@RequestBody Blog blog) {
         Blog newBlog = blogService.updateBlogs(blog);
         return new ResponseEntity<>(newBlog, HttpStatus.OK);
     }
 
+
+    // @api:           "blog/delete/:id"
+    // @desc:         "Api to delete a blog with the ID Provided"
+    // @access_type:  "Public"
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> deleteBlog(@PathVariable("id") Long id) {
         // blogService.findBlogById(id);
-        Void deletedResult = blogService.deleteBlog(id);
+         blogService.deleteBlog(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
--- src/main/java/com/blogs/finalBlogs/models/service/BlogService.java
+++ src/main/java/com/blogs/finalBlogs/models/service/BlogService.java
@@ -22,9 +22,10 @@ public class BlogService {
     }
 
 
-
-// title: null,
-// title: ""
+    /**
+     * @param blog
+     * @return 
+     */
     public Blog addBlog(Blog blog) {
         if((blog.getTitle() == null || blog.getTitle().length() == 0) && (blog.getDescription() == null || blog.getDescription().length() == 0)) {
             throw new BadRequest("title and description both cannot be null!");
@@ -41,21 +42,29 @@ public class BlogService {
 
 
 
-
+    /**
+     * @return
+     */
     public List<Blog> getBlogs() {
         return blogRepo.findAll();
     }
 
+    /**
+     * 
+     * @param blog
+     * @return
+     */
     public Blog updateBlogs(Blog blog) {
         if(blog.getId() == null) {
             throw new ResourceNotFoundException("Id Cannot be null!");
         }   else if(blog.getTitle() == null || blog.getTitle().length() == 0) {
             throw new BadRequest("title cannot be of type null or empty string!");
-        } else if(blog.getDescription() == null || blog.getDescription().length() == 0) {
+        } else if(blog.getDescription() == null || blog.getDescription().isEmpty() || blog.getDescription().length() == 0) {
             throw new BadRequest("description cannot be of type null or empty string!");
         } else if (blog.getImg_url() == null) {
-            throw new BadRequest("img_url cannot be null!");
+            throw new BadRequest("img_url cannot be of type null!");
         } 
+
         else {
             blogRepo.findBlogById(blog.getId()).orElseThrow(() -> new ResourceNotFoundException("Blog with id "+ blog.getId() +" does not exist!"));
         }
@@ -67,10 +76,15 @@ public class BlogService {
     
         }
 
+    /**
+     * @param id
+     */
     @Transactional
     public void deleteBlog(Long id) {
         blogRepo.findBlogById(id).orElseThrow(() -> new ResourceNotFoundException("Blog with id "+ id +" does not exist!"));
         blogRepo.deleteBlogById(id);
     }
 }
-    
+
+
+
